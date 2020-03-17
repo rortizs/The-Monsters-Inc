473ee7650c15ce3783e73d41830addf8a3191ed3
Mynor98 <60805444+Mynor98@users.noreply.github.com>
2020-03-17 16:10:33 -0600

    Login
    a continuacion adjunto el login con su imagen y estilo css a utilizar

--- vista/index.php
+++ vista/index.php
@@ -1,91 +1,49 @@
 <!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/7e73ebaf62.js" crossorigin="anonymous"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="icon" type="image/png" href="img/logo.png" style="max-width:100%;width:100px;height:auto; text-align:left;" />
-	<title> servimedi </title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-  
-</head>
-<body>
-	
-<div class="container-fluid bg-secondary">
-	
-	<img src="img/logo2.png" style="max-width:30%;">
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/stylelog.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <link rel="icon" type="image/png" href="img/logo.png" style="max-width:100%;width:100px;height:auto; text-align:left;" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</div>
+  </head>
+  <body>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-info sticky-top">
-  <a class="navbar-brand" href="#"><i class="fas fa-home"></i> Inicio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"><i class="fab fa-buffer"></i> 
-          Movimientos
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><i class="fas fa-cart-plus"></i> Compras</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#"><i class="fas fa-truck"></i> Ventas</a>
+      <form action="principal.html" class="login-form">
+        
+        <div class="bottom-text">
+         <img src="img/logo2.png" style="max-width:85%;width:auto;height:auto; text-align:center;">
         </div>
-      </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"><i class="fas fa-box-open"></i> 
-          Inventario
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><i class="fas fa-barcode"></i> Productos</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#"><i class="fab fa-stack-overflow"></i> Categorias</a>
+        <div class="txtb">
+          <input type="text">
+          <span data-placeholder="Usuario"></span>
         </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"><i class="far fa-folder-open"></i> Reportes</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><i class="far fa-file"></i></i> Reporte Compras</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#"><i class="far fa-file"></i> Reporte Ventas</a>
+
+        <div class="txtb">
+          <input type="password">
+          <span data-placeholder="Constaseña"></span>
         </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"><i class="fas fa-user"></i> 
-          Personas
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Clientes</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Proveedores</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-danger" href="#"><i class="fas fa-sign-out-alt"></i> Salir</a>
-      </li>
-    </ul>
-  </div>
-</nav>
- <div class="cont">
- 
 
-<!-- area para colocar nuevo codigo -->
+        <input type="submit" class="logbtn" value="Iniciar" >
+
+        
+
+      </form>
+
+      <script type="text/javascript">
+      $(".txtb input").on("focus",function(){
+        $(this).addClass("focus");
+      });
+
+      $(".txtb input").on("blur",function(){
+        if($(this).val() == "")
+        $(this).removeClass("focus");
+      });
+
+      </script>
 
 
-
-
-
-
-
-
-</div>
-</body>
-
+  </body>
 </html>
