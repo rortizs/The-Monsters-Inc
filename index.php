8052c45623666094eebbb7cc21896ae564025bba
Nsanchez29 <60805500+Nsanchez29@users.noreply.github.com>
2020-05-23 19:57:59 -0600

    ..

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once "controladores/productos.controlador.php";
 require_once "controladores/clientes.controlador.php";
 require_once "controladores/ventas.controlador.php";
 require_once "controladores/proveedores.controlador.php";
-require_once "controladores/contactos.controlador.php";
+
 
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/categorias.modelo.php";
@@ -15,7 +15,7 @@ require_once "modelos/productos.modelo.php";
 require_once "modelos/clientes.modelo.php";
 require_once "modelos/ventas.modelo.php";
 require_once "modelos/proveedores.modelo.php";
-require_once "modelos/contactos.modelo.php";
+
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
