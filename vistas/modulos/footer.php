d94a41afce331d413ea9c5bdcf07d7bb211de31b
Mynor98 <60805444+Mynor98@users.noreply.github.com>
2020-05-23 19:24:49 -0600

    complementos
    se agregan partes de las plantillas y archivo para los reportes del sistema

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+
+<strong>Copyright &copy; 2020 <a href="http://tcsogt.com">TCSOGT</a>.</strong>
+    Todos los Derechos Reservados.
+
+	<span class="pull-right footer hidden-xs hidden-sm">
+
+	<b>Version</b> 1.0
+
+	</span>
+
+	
+</footer>
