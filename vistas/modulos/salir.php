369f954742293ec2b00ebcc8d32602dc1bf7e6ac
Nsanchez29 <60805500+Nsanchez29@users.noreply.github.com>
2020-06-05 02:49:25 -0600

    Examen Final
    Cambios al archivo salir.php en base a la creacion de la bitacora de hora de salida de sesion.

--- vistas/modulos/salir.php
+++ vistas/modulos/salir.php
@@ -1,9 +1,38 @@
 <?php
 
-session_destroy();
+echo $_SESSION['id'];
+echo $_SESSION['usuario'];
 
-echo '<script>
 
-	window.location = "ingreso";
+date_default_timezone_set('America/Guatemala');
+			
+	$fecha = date('Y-m-d');
+	$hora = date('H:i:s');
+		
+	$fechaActual = $fecha.' '.$hora;
+$stmt = Conexion::conectar()->prepare("INSERT INTO tbl_logsalida(idusuario, fin_login) VALUES (:id,:fechaActual)");
 
-</script>';
+		$stmt->bindParam(":id", $_SESSION['id'], PDO::PARAM_STR);
+		$stmt->bindParam(":fechaActual", $fechaActual, PDO::PARAM_STR);
+		if($stmt->execute()){
+
+			session_destroy();							
+			echo '<script>
+											
+				window.location = "ingreso";
+					
+			</script>';
+
+		}else{
+
+			return "error";
+		
+		}
+
+		$stmt->close();
+		
+		$stmt = null;
+
+
+
+?>
